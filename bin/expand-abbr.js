cb68ec7b87fc5cbc82fb379dc15b46f5b3ed59a2
kazhashimoto <zkar.h62@gmail.com>
2021-08-30 15:41:26 +0900

    Add wrapper option

--- bin/expand-abbr.js
+++ bin/expand-abbr.js
@@ -8,11 +8,28 @@ program
   .name('expand-abbr')
   .version('1.0.0')
   .usage('[options] abbreviation ...')
-  .showHelpAfterError();
+  .showHelpAfterError()
+  .option('-w,--wrapper <parent>', 'wrap expanded elements with parent')
 
 program.parse(process.argv);
-// const options = program.opts();
+const options = program.opts();
 
-program.args.forEach(abbr => {
-  console.log(expand(abbr));
-});
+function concat(abbr_list) {
+  let expression = '';
+  abbr_list.forEach(abbr => {
+    if (expression) {
+      expression += '+';
+    }
+    expression += `(${abbr})`;
+  });
+  return expression;
+}
+
+if (options.wrapper) {
+  // console.log(expand(options.wrapper));
+  console.log(expand(options.wrapper + '>' + concat(program.args)));
+} else {
+  program.args.forEach(abbr => {
+    console.log(expand(abbr));
+  });
+}
