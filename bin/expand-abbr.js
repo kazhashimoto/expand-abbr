5a4003c03a4c718eb3ab267bc1f86874b00b8160
kazhashimoto <zkar.h62@gmail.com>
2021-08-30 12:18:49 +0900

    Initial commit

---
+++ bin/expand-abbr.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const { program } = require("commander");
+const emmet = require('emmet');
+const expand = emmet.default;
+
+program
+  .name('expand-abbr')
+  .version('1.0.0')
+  .usage('[options] abbreviation ...')
+  .showHelpAfterError();
+
+program.parse(process.argv);
+const options = program.opts();
+
+program.args.forEach(abbr => {
+  console.log(expand(abbr));
+});
