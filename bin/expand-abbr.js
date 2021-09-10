9013376edef9ecc112e80f4462736e3536217803
kazhashimoto <zkar.h62@gmail.com>
2021-09-11 01:47:25 +0900

    Update version

--- bin/expand-abbr.js
+++ bin/expand-abbr.js
@@ -6,7 +6,7 @@ const expand = emmet.default;
 
 program
   .name('expand-abbr')
-  .version('1.0.2')
+  .version('1.0.3')
   .usage('[options] abbreviation ...')
   .showHelpAfterError()
   .option('-h,--head', 'prepend html header')
