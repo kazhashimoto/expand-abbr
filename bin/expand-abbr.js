12caaf585546d6abe30fbcec495eb1ac224f4127
kazhashimoto <zkar.h62@gmail.com>
2023-04-25 00:15:50 +0900

    Start 1.1.11 branch

--- bin/expand-abbr.js
+++ bin/expand-abbr.js
@@ -37,7 +37,7 @@ function collect(value, previous) {
 
 program
   .name('expand-abbr')
-  .version('1.1.10')
+  .version('1.1.11')
   .usage('[options] abbreviation ...')
   .showHelpAfterError()
   .option('-h,--head', 'prepend html header')
